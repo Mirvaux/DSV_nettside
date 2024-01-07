4ac2f1d190aa5e2ccd5b4150efd8c1e1d6d6b476
Mirvaux <larsmfu@gmail.com>
2024-01-07 01:27:13 +0100

    logo working?

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -16,6 +16,15 @@ export default function BlogHeader({
     case 1:
       return (
         <header className="mb-10 mt-16 flex flex-col items-center md:mb-12 md:flex-row md:justify-between">
+          <Image
+          className="h-auto w-full"
+          width={2000}
+          height={1000}
+          alt=""
+          src="/logo_w.png"
+          sizes="100vw"
+          priority={priority}
+          />
           <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
             {title}
           </h1>
